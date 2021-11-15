0afd572d5c8196a352e0843bd230662a7c8252bf
Marion Desbas <desbasmarion@yahoo.fr>
2021-11-15 12:54:53 +0100

    Path correction css 🎨

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './styles/index.scss';
+import './styles/index.css';
 
 //Index page only used to call the entire application
 
